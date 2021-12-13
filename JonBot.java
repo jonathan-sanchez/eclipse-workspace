a490947361d27e2b3b16a3dc758e17efebcb43da
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-12-13 14:30:31 -0600

    feat: added response

--- JonBot.java
+++ JonBot.java
@@ -2,15 +2,40 @@ import java.util.Scanner;
 
 public class JonBot {
 	public static void main(String[] args) {
+		
         System.out.println("Hey Beautiful! This is the bubba bot!");
         talkToJon();
     }
 
-    public static void talkToJon() {
-        boolean keepTalking;
+    public static boolean talkToJon() {
+    	
+        boolean keepTalking = true;
 
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("As Jon walks in with his head held high looks at you and says 'Hey beautiful!'");
+        
         System.out.println("How's your day?!");
+        
+        String hannahInput = sc.nextLine();
+        
+        if (hannahInput.endsWith("?")) {
+        		System.out.println("Fuck Yeah!");
+        } else {
+        		System.out.println("I dont understand that question babe.");
+        }
+        
+        System.out.println("Anything else babe? [yes/no]");
+        
+        String stillTalking = sc.nextLine();
+        
+        for (int i = 0; i < stillTalking.length(); i++) {
+        		if (hannahInput.equalsIgnoreCase("yes")) {
+        			return true;
+        		} else if(hannahInput.equalsIgnoreCase("no")) {
+        			return false;
+        		}
+        }
+        return talkToJon();
     }
 }
